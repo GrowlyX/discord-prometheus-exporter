4e7a526175d77e98264b922bcb4f00b7a55b940d
GrowlyX <growlyxdev@gmail.com>
2022-08-25 13:40:52 -0400

    Configure kord

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,26 @@ plugins {
     application
 }
 
-group = "gg.growly.discoprom"
+group = "io.liftgate.discord.exporter"
 version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.kotlindiscord.com/repository/maven-public/")
 }
 
 dependencies {
     api(kotlin("stdlib"))
+    api("io.prometheus:simpleclient_httpserver:0.16.0")
+
+    api("dev.kord:kord-core:0.8.0-M16")
+    api("com.kotlindiscord.kord.extensions:kord-extensions:1.5.2-RC1")
+
+    api("com.xenomachina:kotlin-argparser:2.0.7")
 }
 
 application {
-    mainClass.set("gg.growly.discoprom.ApplicationKt")
+    mainClass.set("io.liftgate.discord.exporter.ApplicationKt")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/io/liftgate/discord/exporter/application.kt
@@ -0,0 +1,53 @@
+package io.liftgate.discord.exporter
+
+import com.kotlindiscord.kord.extensions.ExtensibleBot
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
+import dev.kord.core.supplier.EntitySupplyStrategy
+import dev.kord.gateway.Intents
+import dev.kord.gateway.PrivilegedIntent
+import io.prometheus.client.exporter.HTTPServer
+
+/**
+ * @author GrowlyX
+ * @since 8/25/2022
+ */
+@OptIn(PrivilegedIntent::class)
+suspend fun main(vararg args: String)
+{
+    class AppConfiguration(parser: ArgParser)
+    {
+        val token by parser.storing(
+            "--token", help = "Discord bot token."
+        )
+
+        val bindAddress by parser
+            .storing(
+                "--addr", help = "Address on which the HTTP server will listen ojn."
+            )
+            .default("0.0.0.0:9800")
+    }
+
+    val configuration = ArgParser(args)
+        .parseInto(::AppConfiguration)
+
+    val extensible = ExtensibleBot(configuration.token) {
+        intents {
+            this.flags().plus(Intents.all)
+        }
+
+        cache {
+            cachedMessages = 10_000
+            defaultStrategy = EntitySupplyStrategy.cacheWithCachingRestFallback
+        }
+    }
+
+    val bindAddress = configuration
+        .bindAddress.split(":")
+
+    val server = HTTPServer(
+        bindAddress.first(), bindAddress[1].toInt()
+    )
+
+
+}
